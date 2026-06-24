14e45400218d5b0276a067b37d644f28266b2795
Amodamendis <amodarashmika2003@gmail.com>
2026-06-24 20:03:18 +0530

    Added nsg/network security groups (all port configs )

---
+++ accounts/dev-account/centralindia/dev/nsg/main.tf
@@ -0,0 +1,55 @@
+variable "location" {}
+variable "resource_group_name" {}
+variable "environment" {}
+variable "project" {}
+
+resource "azurerm_network_security_group" "this" {
+  name                = "shalotrack-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTPS"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    Project     = var.project
+    Environment = var.environment
+  }
+}
+
+output "nsg_id" {
+  value = azurerm_network_security_group.this.id
+}
